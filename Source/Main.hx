0f9efd6a243ba61c88753d9e005bde8675e9d079
leocavalcante <lc@leocavalcante.com>
2017-08-29 21:29:20 -0300

    Add gravity, space and enterFrame. Bare bones

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,19 +1,27 @@
 package;
 
-
+import nape.geom.Vec2;
+import nape.space.Space;
 import openfl.display.Sprite;
+import openfl.events.Event;
 
-
-class Main extends Sprite {
+class Main extends Sprite
+{
+	private var gravity: Vec2;
+	private var space: Space;
 	
-	
-	public function new () {
+	public function new()
+	{
+		super();
 		
-		super ();
+		gravity = new Vec2(0, 1000);
+		space = new Space(gravity);
 		
-		
-		
+		stage.addEventListener(Event.ENTER_FRAME, handleEnterFrame, false, 0, true);
 	}
 	
-	
+	private function handleEnterFrame(event: Event)
+	{
+		space.step(1 / stage.frameRate);
+	}
 }
