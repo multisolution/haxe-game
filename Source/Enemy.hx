bca854e0aff1833e0008bda02fd0cc497af48cf0
eudangeld <eudangeld@gmail.com>
2017-09-05 13:59:19 -0300

    Fix zombie (moonwalk), added tombstone frame, added kill function in enemy

--- Source/Enemy.hx
+++ Source/Enemy.hx
@@ -5,16 +5,27 @@ import nape.shape.Polygon;
 import openfl.Assets;
 import openfl.display.DisplayObject;
 import openfl.display.MovieClip;
+import nape.dynamics.InteractionFilter;
 
 class Enemy extends Entity
 {
 	public var speed: Float = 50;
+    public var isDead = false;
 
 	public function move()
 	{
-		body.velocity.x = speed;
+        body.velocity.x = speed;
 		cast(display, MovieClip).play();
 	}
+
+    public function kill()
+    {
+        cast(display,MovieClip).gotoAndStop("tombstone");
+        body.velocity.x = speed = 0;
+        shape.sensorEnabled = true;
+        isDead = true;
+    }
+
 
 	public function bounce()
 	{
@@ -29,7 +40,7 @@ class Enemy extends Entity
 		height = 27;
 
 		speed = 50 + Data.score();
-		type = BodyType.DYNAMIC;
+		type = BodyType.KINEMATIC;
 		verts = Polygon.box(width, height);
 	}
 
@@ -40,7 +51,6 @@ class Enemy extends Entity
 		body.cbTypes.add(CbTypes.ENEMY);
 		body.allowRotation = false;
 		body.userData.entity = this;
-
 		shape.material.dynamicFriction = 0;
 	}
 
